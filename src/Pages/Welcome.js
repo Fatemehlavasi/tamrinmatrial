3ff7a9f4f514fde1f15c13f147eaf36bbd9e4bf5
FatemehLavasi <111527593+Fatemehlavasi@users.noreply.github.com>
2022-10-30 01:44:36 -0700

    Create a welcome page

--- src/Pages/Welcome.js
+++ src/Pages/Welcome.js
@@ -1,9 +1,32 @@
 import React from 'react'
-
-function Welcome() {
+import Grid from "@mui/material/Grid";
+import ListOption from "../Components/ListOption"
+import profile from '../Image/profile.png'
+import clock from "../Image/clock.png"
+import AddCircleIcon from '@mui/icons-material/AddCircle'
+// import {Button, Typography} from "@mui/material";
+import { Typography,TextField,Box, ListItem,Icon } from '@mui/material';
+function welcome() {
   return (
-    <div>Welcome</div>
+    
+    <>
+    <Box height={300} width={'100%'} bgcolor={'primary.main'} display={'flex'} justifyContent={'space-evenly'} alignItems={'center'} sx={{flexDirection:'column'}}>
+        <Box width={'fit-content'}><img src={profile} /></Box>
+        <Box ><Typography variant={'h3'} color={'#000'}>Welcome Oliva Grace</Typography> </Box>
+    </Box>
+    <Grid display={'flex'} sx={{flexDirection:"column",alignItems:'center'}} xs={12}  p={3}>
+        <Grid alignSelf={'flex-end'} ><Typography variant={'h2'} color={'#000'}>Good Afternoon</Typography></Grid>
+        <Grid ><img src={clock} /></Grid>
+        <Grid alignSelf={'flex-start'}><Typography variant={'h2'} color={'#000'}>Task Lists</Typography></Grid>
+        <Grid width={'100%'} bgcolor={'#fff'} height={300} p={1} marginTop={1} borderRadius={5}>
+            <Grid display={'flex'} justifyContent={'space-between'}  sx={{px:2,marginBottom:1}}>               <Typography variant={'h3'}>Daily Tasks</Typography>
+            <AddCircleIcon color='primary'sx={{cursor:'pointer'}}/>
+            </Grid>
+            <ListOption/>
+            </Grid>
+    </Grid>
+    </>
   )
 }
 
-export default Welcome
+export default welcome
